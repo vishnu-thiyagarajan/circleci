e7f2d763237f8b8695d4d85a232f327e7a56b592
vishnu-thiyagarajan <vishnu.thg@gmail.com>
2020-04-07 09:53:24 +0530

    edited test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+// import React from 'react'
+// import { render } from '@testing-library/react'
+// import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />)
+//   const linkElement = getByText(/learn react/i)
+//   expect(linkElement).toBeInTheDocument()
+// })
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+test('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+})
